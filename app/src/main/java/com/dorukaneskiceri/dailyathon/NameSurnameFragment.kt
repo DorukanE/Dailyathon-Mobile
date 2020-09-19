c616e7049e0d0165a5acb42bc3d409b3e92a1cfd
DorukanE <dorukaneskiceri@gmail.com>
2020-09-19 19:17:32 +0300

    feat: NameSurname fragments created

---
+++ app/src/main/java/com/dorukaneskiceri/dailyathon/NameSurnameFragment.kt
@@ -0,0 +1,23 @@
+package com.dorukaneskiceri.dailyathon
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_name_surname.*
+
+class NameSurnameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_name_surname, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/dorukaneskiceri/dailyathon/SignUpActivity.kt
+++ app/src/main/java/com/dorukaneskiceri/dailyathon/SignUpActivity.kt
@@ -3,6 +3,9 @@ package com.dorukaneskiceri.dailyathon
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -11,9 +14,11 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         setSupportActionBar(customToolbar)
+
         customToolbar.setNavigationOnClickListener {
             val intent = Intent(it.context,LoginSignUpActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
