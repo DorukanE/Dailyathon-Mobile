f734e2b249f4f001a633140d540748f7c228972b
DorukanE <dorukaneskiceri@gmail.com>
2020-10-12 19:19:28 +0300

    feat: Pharmacy details added

--- app/src/main/java/com/dorukaneskiceri/dailyathon/fragmentsMain/fragmentsPharmacy/FragmentPharmacy.kt
+++ app/src/main/java/com/dorukaneskiceri/dailyathon/fragmentsMain/fragmentsPharmacy/FragmentPharmacy.kt
@@ -1,17 +1,23 @@
 package com.dorukaneskiceri.dailyathon.fragmentsMain.fragmentsPharmacy
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dorukaneskiceri.dailyathon.R
 import com.dorukaneskiceri.dailyathon.items.PharmacyItems
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
+import kotlinx.android.synthetic.main.dialog_layout.*
 import kotlinx.android.synthetic.main.fragment_pharmacy.*
+import java.net.URI
 
 class FragmentPharmacy : Fragment() {
 
@@ -24,6 +30,8 @@ class FragmentPharmacy : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        var number: String? = ""
 
         imageButtonBackCurrency.setOnClickListener {
             val action = FragmentPharmacyDirections.actionFragmentPharmacyToDestinationDailyathon()
@@ -42,6 +50,29 @@ class FragmentPharmacy : Fragment() {
         adapter.add(PharmacyItems())
         adapter.add(PharmacyItems())
         adapter.add(PharmacyItems())
+
+        adapter.setOnItemClickListener { item, view ->
+            val dialog = BottomSheetDialog(view.context)
+            val view = layoutInflater.inflate(R.layout.dialog_layout,null)
+            dialog.setContentView(view)
+            dialog.show()
+
+            val textViewCancel: TextView = view.findViewById(R.id.textViewCancel)
+            val textViewMakeCall: TextView = view.findViewById(R.id.textViewMakeCall)
+            val textViewFind: TextView = view.findViewById(R.id.textViewFind)
+
+            textViewCancel.setOnClickListener {
+                dialog.dismiss()
+            }
+            textViewFind.setOnClickListener {
+
+            }
+            textViewMakeCall.setOnClickListener {
+                number = "+905319409022"
+                val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + Uri.encode(number)))
+                startActivity(intent)
+            }
+        }
     }
 
 }
