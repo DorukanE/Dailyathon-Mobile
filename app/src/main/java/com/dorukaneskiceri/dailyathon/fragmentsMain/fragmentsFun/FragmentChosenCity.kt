a4af3e6feb3ac6317bf45d2a65b846c7db815ff0
DorukanE <dorukaneskiceri@gmail.com>
2020-10-14 18:30:54 +0300

    feat: ChosenCity screen completed

--- app/src/main/java/com/dorukaneskiceri/dailyathon/fragmentsMain/fragmentsFun/FragmentChosenCity.kt
+++ app/src/main/java/com/dorukaneskiceri/dailyathon/fragmentsMain/fragmentsFun/FragmentChosenCity.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dorukaneskiceri.dailyathon.R
+import com.dorukaneskiceri.dailyathon.items.ChosenCityItems
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import kotlinx.android.synthetic.main.fragment_chosen_city.*
 
 class FragmentChosenCity : Fragment() {
 
@@ -16,4 +21,15 @@ class FragmentChosenCity : Fragment() {
         return inflater.inflate(R.layout.fragment_chosen_city, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerViewChosenCity.layoutManager = LinearLayoutManager(view.context)
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        recyclerViewChosenCity.adapter = adapter
+
+        for(i in 1..8){
+            adapter.add(ChosenCityItems())
+        }
+    }
 }
