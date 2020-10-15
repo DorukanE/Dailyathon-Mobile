c991adb7271fd727e226f11847c4fd298a00a6df
DorukanE <dorukaneskiceri@gmail.com>
2020-10-15 15:39:19 +0300

    feat: Forgot password page completed

--- app/src/main/java/com/dorukaneskiceri/dailyathon/login_signup/ForgotPasswordActivity.kt
+++ app/src/main/java/com/dorukaneskiceri/dailyathon/login_signup/ForgotPasswordActivity.kt
@@ -3,30 +3,23 @@ package com.dorukaneskiceri.dailyathon.login_signup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dorukaneskiceri.dailyathon.activity.MainAppActivity
 import com.dorukaneskiceri.dailyathon.R
+import kotlinx.android.synthetic.main.activity_forgot_password.*
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_forgot_password)
 
-        setSupportActionBar(customToolbarLogin)
-
-        textViewForgotPsw.setOnClickListener{
-            val intent = Intent(it.context,ForgotPasswordActivity::class.java)
-            startActivity(intent)
-        }
-
-        textViewBackToSignup.setOnClickListener {
+        backToSignUpForgot.setOnClickListener {
             val intent = Intent(it.context,SignUpActivity::class.java)
             startActivity(intent)
             finish()
         }
 
-        loginIntoAppButton.setOnClickListener {
-            val intent = Intent(it.context, MainAppActivity::class.java)
+        imageBackButtonPsw.setOnClickListener {
+            val intent = Intent(it.context,LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
