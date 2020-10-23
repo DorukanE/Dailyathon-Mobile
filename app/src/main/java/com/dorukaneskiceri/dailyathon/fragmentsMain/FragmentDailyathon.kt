7d3b770fd0ae2f91b71b688dbab326dc308c53da
DorukanE <dorukaneskiceri@gmail.com>
2020-10-23 19:04:17 +0300

    feat: Movit api connected

--- app/src/main/java/com/dorukaneskiceri/dailyathon/fragmentsMain/FragmentDailyathon.kt
+++ app/src/main/java/com/dorukaneskiceri/dailyathon/fragmentsMain/FragmentDailyathon.kt
@@ -1,14 +1,18 @@
 package com.dorukaneskiceri.dailyathon.fragmentsMain
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.dorukaneskiceri.dailyathon.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.fragment_dailyathon.*
+
 
 class FragmentDailyathon : Fragment() {
 
@@ -24,19 +28,44 @@ class FragmentDailyathon : Fragment() {
 
         cardViewPharmacy.setOnClickListener {
             hideNavigationBar()
-            val action = FragmentDailyathonDirections.actionDestinationDailyathonToFragmentPharmacy()
+            val action =
+                FragmentDailyathonDirections.actionDestinationDailyathonToFragmentPharmacy()
             Navigation.findNavController(it).navigate(action)
         }
 
         cardViewCurrency.setOnClickListener {
             hideNavigationBar()
-            val action = FragmentDailyathonDirections.actionDestinationDailyathonToFragmentCurrency()
+            val action =
+                FragmentDailyathonDirections.actionDestinationDailyathonToFragmentCurrency()
             Navigation.findNavController(it).navigate(action)
+        }
+
+        cardViewMovit.setOnClickListener {
+            openMovitPage()
+        }
+    }
+
+    private fun openMovitPage() {
+        try {
+            val packageManager = view?.context?.packageManager
+            packageManager?.getPackageInfo("com.tranzmate", PackageManager.GET_ACTIVITIES)
+            //"moovit://directions?dest_lat=&dest_lon=&dest_name=&orig_lat=&orig_lon=&orig_name=&date=2020-10-23T17:48:00+03:00&partner_id=com.dorukaneskiceri.dailyathon"
+            val uri = "moovit://nearby?lat=&lon=&partner_id=com.dorukaneskiceri.dailyathon>"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(uri)
+            startActivity(intent)
+        } catch (e: PackageManager.NameNotFoundException) {
+            val url = "http://app.appsflyer.com/com.tranzmate?pid=DL&c=com.dorukaneskiceri.dailyathon"
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
+
         }
     }
 
     private fun hideNavigationBar() {
-        val bottomNavigationBar = requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavMainApp)
+        val bottomNavigationBar =
+            requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavMainApp)
         bottomNavigationBar.visibility = View.GONE
     }
 
