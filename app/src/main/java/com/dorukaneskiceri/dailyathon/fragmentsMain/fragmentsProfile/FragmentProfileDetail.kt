7294d1215c062aa31a410f49679d90c060ef97b3
DorukanE <dorukaneskiceri@gmail.com>
2020-11-13 20:54:26 +0300

    fix: Class name changed

--- app/src/main/java/com/dorukaneskiceri/dailyathon/fragmentsMain/fragmentsProfile/FragmentProfileDetail.kt
+++ app/src/main/java/com/dorukaneskiceri/dailyathon/fragmentsMain/fragmentsProfile/FragmentProfileDetail.kt
@@ -8,16 +8,15 @@ import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.navigation.Navigation
 import com.dorukaneskiceri.dailyathon.R
-import com.dorukaneskiceri.dailyathon.fragmentsMain.FragmentNewsDirections
-import kotlinx.android.synthetic.main.fragment_profile_screen.*
+import kotlinx.android.synthetic.main.fragment_profile_detail.*
 
-class FragmentProfileScreen : Fragment() {
+class FragmentProfileDetail : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile_screen, container, false)
+        return inflater.inflate(R.layout.fragment_profile_detail, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,20 +26,20 @@ class FragmentProfileScreen : Fragment() {
             viewLifecycleOwner,
             object : OnBackPressedCallback(true) {
                 override fun handleOnBackPressed() {
-                    val action = FragmentProfileScreenDirections.actionFragmentProfileScreenToDestinationProfile()
+                    val action = FragmentProfileDetailDirections.actionFragmentProfileScreenToDestinationProfile()
                     Navigation.findNavController(view).navigate(action)
                 }
             })
 
         arguments?.let {
-            val categoryName = FragmentProfileScreenArgs.fromBundle(it).categoryName
-            val categoryID = FragmentProfileScreenArgs.fromBundle(it).categoryID
+            val categoryName = FragmentProfileDetailArgs.fromBundle(it).categoryName
+            val categoryID = FragmentProfileDetailArgs.fromBundle(it).categoryID
 
             textView20.text = categoryName
         }
 
         imageView19.setOnClickListener {
-            val action = FragmentProfileScreenDirections.actionFragmentProfileScreenToDestinationProfile()
+            val action = FragmentProfileDetailDirections.actionFragmentProfileScreenToDestinationProfile()
             Navigation.findNavController(it).navigate(action)
         }
     }
