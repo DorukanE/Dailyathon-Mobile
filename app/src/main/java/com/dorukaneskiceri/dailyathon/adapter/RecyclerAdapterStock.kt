a14fd8c279a55f5b89bc124736e451f19a1aae59
DorukanE <dorukaneskiceri@gmail.com>
2020-11-16 21:04:27 +0300

    feat: Stock screen changed and data binding completed

--- app/src/main/java/com/dorukaneskiceri/dailyathon/adapter/RecyclerAdapterStock.kt
+++ app/src/main/java/com/dorukaneskiceri/dailyathon/adapter/RecyclerAdapterStock.kt
@@ -3,8 +3,10 @@ package com.dorukaneskiceri.dailyathon.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.dorukaneskiceri.dailyathon.R
+import com.dorukaneskiceri.dailyathon.databinding.RecyclerViewStockBinding
 import com.dorukaneskiceri.dailyathon.model.api_model.StockListModel
 import kotlinx.android.synthetic.main.recycler_view_stock.view.*
 
@@ -12,21 +14,19 @@ class RecyclerAdapterStock(private val arrayList: ArrayList<StockListModel>): Re
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StockHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.recycler_view_stock,parent,false)
+        val view = DataBindingUtil.inflate<RecyclerViewStockBinding>(inflater, R.layout.recycler_view_stock, parent, false)
         return StockHolder(view)
     }
 
     override fun onBindViewHolder(holder: StockHolder, position: Int) {
-        holder.view.textViewTitleStock.text = arrayList.get(position).stockText
-        holder.view.textViewDescriptionStock.text = arrayList.get(position).stockCode
-        holder.view.textViewChangeRateStock.text = arrayList.get(position).stockLastPrice
+        holder.view.stock = arrayList.get(position)
     }
 
     override fun getItemCount(): Int {
         return arrayList.size
     }
 
-    class StockHolder(var view: View): RecyclerView.ViewHolder(view){
+    class StockHolder(var view: RecyclerViewStockBinding): RecyclerView.ViewHolder(view.root){
 
     }
 }
