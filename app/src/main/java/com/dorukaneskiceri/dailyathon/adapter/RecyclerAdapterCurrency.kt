6d2e0c4b341e03ab1344174aa062f63c41021454
DorukanE <dorukaneskiceri@gmail.com>
2020-11-16 18:59:35 +0300

    feat: Currency Screen design changed

--- app/src/main/java/com/dorukaneskiceri/dailyathon/adapter/RecyclerAdapterCurrency.kt
+++ app/src/main/java/com/dorukaneskiceri/dailyathon/adapter/RecyclerAdapterCurrency.kt
@@ -3,14 +3,12 @@ package com.dorukaneskiceri.dailyathon.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dorukaneskiceri.dailyathon.R
-import com.dorukaneskiceri.dailyathon.model.CurrencyModel
 import com.dorukaneskiceri.dailyathon.model.api_model.CurrencyListModel
 import kotlinx.android.synthetic.main.recycler__view_currency.view.*
 
-class RecyclerAdapterCurrency(private val currencyModel: ArrayList<CurrencyListModel>): RecyclerView.Adapter<RecyclerAdapterCurrency.CurrencyHolder>() {
+class RecyclerAdapterCurrency(private val arrayListCurrency: ArrayList<CurrencyListModel>): RecyclerView.Adapter<RecyclerAdapterCurrency.CurrencyHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrencyHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -19,13 +17,15 @@ class RecyclerAdapterCurrency(private val currencyModel: ArrayList<CurrencyListM
     }
 
     override fun onBindViewHolder(holder: CurrencyHolder, position: Int) {
-        holder.view.textViewTitleCurrency.text = currencyModel.get(position).currencyCode
-        holder.view.textViewDescriptionCurrency.text = currencyModel.get(position).currencyName
-        holder.view.textViewChangeRateCurrency.text = currencyModel.get(position).currencyBuyValue
+        holder.view.textViewChangeRateCurrency.text = arrayListCurrency.get(position).currencyRate
+        holder.view.textViewTitleCurrency.text = arrayListCurrency.get(position).currencyCode
+        holder.view.textViewDescriptionCurrency.text = arrayListCurrency.get(position).currencyName
+        holder.view.textViewBuyValue.text = arrayListCurrency.get(position).currencyBuyValue
+        holder.view.textViewSellValue.text = arrayListCurrency.get(position).currencySellValue
     }
 
     override fun getItemCount(): Int {
-        return currencyModel.size
+        return arrayListCurrency.size
     }
 
     class CurrencyHolder(var view: View): RecyclerView.ViewHolder(view){
