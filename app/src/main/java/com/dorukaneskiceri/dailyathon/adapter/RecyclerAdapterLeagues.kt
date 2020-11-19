7f45d35f27ab73682b5dacb5086096fac7135018
DorukanE <dorukaneskiceri@gmail.com>
2020-11-19 16:04:58 +0300

    feat: League List click listener completed

--- app/src/main/java/com/dorukaneskiceri/dailyathon/adapter/RecyclerAdapterLeagues.kt
+++ app/src/main/java/com/dorukaneskiceri/dailyathon/adapter/RecyclerAdapterLeagues.kt
@@ -1,22 +1,31 @@
 package com.dorukaneskiceri.dailyathon.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.dorukaneskiceri.dailyathon.R
 import com.dorukaneskiceri.dailyathon.databinding.RecyclerViewLeagueListBinding
 import com.dorukaneskiceri.dailyathon.model.LeagueListModel
+import kotlinx.android.synthetic.main.recycler_view_league_list.view.*
 
-class RecyclerAdapterLeagues(private val arrayListLeague: ArrayList<LeagueListModel>): RecyclerView.Adapter<RecyclerAdapterLeagues.LeagueHolder>() {
+class RecyclerAdapterLeagues(private val arrayListLeague: ArrayList<LeagueListModel>) :
+    RecyclerView.Adapter<RecyclerAdapterLeagues.LeagueHolder>(), LeagueListClickListener{
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LeagueHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = DataBindingUtil.inflate<RecyclerViewLeagueListBinding>(inflater, R.layout.recycler_view_league_list, parent, false)
+        val view = DataBindingUtil.inflate<RecyclerViewLeagueListBinding>(
+            inflater,
+            R.layout.recycler_view_league_list,
+            parent,
+            false
+        )
         return LeagueHolder(view)
     }
 
     override fun onBindViewHolder(holder: LeagueHolder, position: Int) {
+        holder.view.listener = this
         holder.view.league = arrayListLeague.get(position)
     }
 
@@ -24,7 +33,12 @@ class RecyclerAdapterLeagues(private val arrayListLeague: ArrayList<LeagueListMo
         return arrayListLeague.size
     }
 
-    class LeagueHolder(var view: RecyclerViewLeagueListBinding): RecyclerView.ViewHolder(view.root){
+    class LeagueHolder(var view: RecyclerViewLeagueListBinding) :
+        RecyclerView.ViewHolder(view.root) {
 
+    }
+
+    override fun onLeagueClicked(it: View) {
+        println(it.textViewSportName.text)
     }
 }
