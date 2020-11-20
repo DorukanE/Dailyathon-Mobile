28b954499a561fc24f9570a983d1a662bf55edde
DorukanE <dorukaneskiceri@gmail.com>
2020-11-20 14:45:14 +0300

    feat: Score List table view completed

--- app/src/main/java/com/dorukaneskiceri/dailyathon/adapter/RecyclerAdapterScores.kt
+++ app/src/main/java/com/dorukaneskiceri/dailyathon/adapter/RecyclerAdapterScores.kt
@@ -1,5 +1,6 @@
 package com.dorukaneskiceri.dailyathon.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -9,6 +10,8 @@ import com.dorukaneskiceri.dailyathon.databinding.RecyclerViewScoreListBinding
 import com.dorukaneskiceri.dailyathon.model.UserLeagueListModel
 
 class RecyclerAdapterScores(private val arrayListScore: ArrayList<UserLeagueListModel>): RecyclerView.Adapter<RecyclerAdapterScores.ScoreListHolder>() {
+
+    private val colors: Array<String> = arrayOf("#5d778f","#626667")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScoreListHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -26,6 +29,7 @@ class RecyclerAdapterScores(private val arrayListScore: ArrayList<UserLeagueList
         holder.view.textViewScore.text = arrayListScore.get(position).score.toString()
         holder.view.textViewPoint.text = arrayListScore.get(position).point.toString()
         holder.view.score = arrayListScore.get(position)
+        holder.itemView.setBackgroundColor(Color.parseColor(colors[position % 2]))
     }
 
     override fun getItemCount(): Int {
